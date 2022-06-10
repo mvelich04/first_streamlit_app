93693a5f612c390c20e45d56bead1ef7164f4342
Maria Velichkova <107260060+mvelich04@users.noreply.github.com>
2022-06-10 17:42:04 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -4,6 +4,7 @@ import streamlit
 streamlit.title ("My parents new healthy dinner")
 
 streamlit.header('Breakfast Favorites')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
